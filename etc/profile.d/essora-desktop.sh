b6517bc8c8795524e1fffff9951f30d185551d10
josejp2424 <josejp2424@gmail.com>
2026-05-12 21:38:05 -0300

    add essora desktop

---
+++ etc/profile.d/essora-desktop.sh
@@ -0,0 +1,5 @@
+# Identifica a Essora como Desktop Environment para apps que detectan
+# XDG_CURRENT_DESKTOP (fastfetch, neofetch, glib, GTK, etc.)
+export XDG_CURRENT_DESKTOP=Essora
+export XDG_SESSION_DESKTOP=essora
+export DESKTOP_SESSION=essora
